03b0a08f114cd4f767aefaa416f1d239e4aa82a2
頼本圭太 <asbo0502@gmail.com>
2020-03-29 00:58:19 +0900

    ABC160 second challenge Prob.C

--- ABC160/ABC160_C.cpp
+++ ABC160/ABC160_C.cpp
@@ -11,48 +11,12 @@ using ld = long double;
 
 int main(){
     ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
-    int k, n;
-    cin >> k >> n;
-
-    int m = 0, index = 0, fi, pre = 0;
-    vector<int> v;
-    forn(i, n){
-        int a; cin >> a;
-        if(i == 0) {
-            fi = a;
-            pre = a;
-            v.push_back(0);
-        }else{
-            int to = abs(pre - a);
-            // cout << "a:" << a << ", fi:" << fi << ", pre:" << pre << ", to:" << to << endl;
-            v.push_back(to);
-            pre = a;
-
-            if(m < to){
-                // cout << "  update m.  to:" << to << ", index:" << i << endl;
-                index = i;
-                m = to;
-            }
-        }
-    }
-    // v.push_back(abs(k - pre) + fi);
-    if(index != 1) v.push_back(abs(k - pre) + fi);
-    int ans = 0;
-
-    // cout << "v:[";
-    // forn(i, v.size()) cout << v[i] << ", ";
-    // cout << "]" << endl;
+    int k, n; cin >> k >> n;
+    vector<int> v(n);
+    forn(i, n) cin >> v[i];
+    v.push_back(k + v[0]);
+    int l = 0;
+    forn(i, v.size()) l = max(l, v[i+1] - v[i]);
     
-    if(index == 1){
-        forn(i, index - 1) ans += v[i];
-        // forn(i, index - 1) cout << "forn:" << v[i] << endl;
-        fore(i, index, v.size() - 1) ans += v[i];
-        // fore(i, index, v.size() - 1) cout << "fore:" << v[i] << endl;
-    }else{
-        forn(i, index) ans += v[i];
-        // forn(i, index) cout << "forn:" << v[i] << endl;
-        fore(i, index + 1, v.size() - 1) ans += v[i];
-        // fore(i, index + 1, v.size() - 1) cout << "fore:" << v[i] << endl;
-    }
-    cout << ans << endl;
+    cout << k - l << endl;
 }
