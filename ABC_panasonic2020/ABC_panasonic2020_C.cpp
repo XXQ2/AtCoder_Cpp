94e5003dbee73ca249565cd7b7d4608b1909a7fa
頼本圭太 <asbo0502@gmail.com>
2020-04-06 08:16:50 +0900

    ABC_panasonic2020 復習

--- ABC_panasonic2020/ABC_panasonic2020_C.cpp
+++ ABC_panasonic2020/ABC_panasonic2020_C.cpp
@@ -3,21 +3,18 @@
 using namespace std;
 using ll = long long;
 
-int main(){
-    ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
-    long double a, b, c;
-    cin >> a >> b >> c;
-
-    long double sqa, sqb, sqc;
-    sqa = sqrtl(a);
-    sqb = sqrtl(b);
-    sqc = sqrtl(c);
-
-    long double sqab = sqa + sqb;
-    // cout << "sqa :" << sqa << endl;
-    // cout << "sqb :" << sqb << endl;
-    // cout << "sqc :" << sqc << endl;
-    // cout << "sqab:" << sqab << endl;
-    if(sqab < sqc) cout << "Yes" << endl;
-    else cout << "No" << endl;
-}
+int main()
+{
+	ll a, b, c;
+	cin>>a>>b>>c;
+	if(c-a-b<=0){
+		cout<<"No"<<endl;
+		return 0;
+	}
+	if(4*a*b<(c-a-b)*(c-a-b)){
+		cout<<"Yes"<<endl;
+	}else{
+		cout<<"No"<<endl;
+	}
+	return 0;
+}
