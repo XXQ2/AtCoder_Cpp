18b9725d04dfc0dc25a04e74b159c8569dd76064
頼本圭太 <asbo0502@gmail.com>
2020-04-09 00:42:53 +0900

    ABC144 C問題 復習

--- ABC144/ABC144_C.cpp
+++ ABC144/ABC144_C.cpp
@@ -9,44 +9,17 @@ using namespace std;
 using ll = long long;
 using ld = long double;
 
-bool IsPrime(int num){
-    if (num < 2) return false;
-    else if (num == 2) return true;
-    else if (num % 2 == 0) return false; // 偶数はあらかじめ除く
-
-    // double sqrtNum = sqrt(num);
-    for (int i = 3; i < num; i += 2){
-        if (num % i == 0){
-            // 素数ではない
-            return false;
-        }
-    }
-
-    // 素数である
-    return true;
-}
-
 int main(){
     ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
     ll n;
     cin >> n;
-    if(IsPrime(n)){
-        cout << n - 1 << endl;
-        return 0;
+    ll ans = 0;
+    ll l = sqrtl(n) + 1; //切り上げ値
+    ford(i, l){
+        if(n%i != 0) continue;
+        ll j = n/i;
+        ans = i + j - 2;
+        break;
     }
-    ll m = n, a = 0;
-    fore(i, 2, n-1){
-        if(IsPrime(i)){
-            ll l=1, r=1001001001001;
-            while(r - l > 1){
-                ll mid = l + ((r - l)/2);
-                if(i * mid <= n) l = mid;
-                else r = mid;
-            }
-            a = i-1 + l-1;
-            if(m > a) m = a;
-            else break;
-        }
-    }
-    cout << a << endl;
+    cout << ans << endl;
 }
