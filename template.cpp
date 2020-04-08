e15d77d8a296a392de136dbff1083033d7145a3a
頼本圭太 <asbo0502@gmail.com>
2020-04-09 00:47:38 +0900

    update template

--- template.cpp
+++ template.cpp
@@ -1,8 +1,9 @@
 #include <bits/stdc++.h>
-#define forn(i, n) for (int i = 0; i < (int)(n); ++i)
-#define for1(i, n) for (int i = 1; i <= (int)(n); ++i)
-#define fore(i, l, r) for (int i = (int)(l); i <= (int)(r); ++i)
-#define ford(i, n) for (int i = (int)(n) - 1; i >= 0; --i)
+#define forn(i, n) for (long long i = 0; i < (long long)(n); ++i)
+#define for1(i, n) for (long long i = 1; i <= (long long)(n); ++i)
+#define fore(i, l, r) for (long long i = (long long)(l); i <= (long long)(r); ++i)
+#define fordn(i, n) for (long long i = (long long)(n) - 1; i >= 0; --i)
+#define ford1(i, n) for (long long i = (long long)(n); i >= 1; --i)
 #define all(x) (x).begin(), (x).end()
 #define PI 3.141592653589793
 using namespace std;
