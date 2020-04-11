337dc5d6a73a14b5ec734c27eb57cd9edd393a58
頼本圭太 <asbo0502@gmail.com>
2020-04-11 10:26:27 +0900

    ABC002 精進 C問題 途中

--- ABC002/ABC002_C.cpp
+++ ABC002/ABC002_C.cpp
@@ -12,5 +12,15 @@ using ld = long double;
 
 int main(){
     ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
-    return 0;
+    vector<pair<ld, ld>> P(3);
+    forn(i, 3) cin >> P[i].first >> P[i].second;
+    ld diffX = (P[0].first >= 0 ? 0 - P[0].first : abs(P[0].first));
+    ld diffY = (P[0].second >= 0 ? 0 - P[0].second : abs(P[0].second));
+
+    forn(i, 3){
+        P[i].first = abs(P[i].first + diffX);
+        P[i].second = abs(P[i].second + diffY);
+    }
+    ld ans = abs(P[1].first*P[2].second - P[1].second*P[2].first) / 2.0;
+    cout << setprecision(5) << ans << endl;
 }
