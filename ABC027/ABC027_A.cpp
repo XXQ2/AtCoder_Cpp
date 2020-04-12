a7583809a6b0878f555a83cefed39cc6e4fbb2ba
頼本圭太 <asbo0502@gmail.com>
2020-04-12 22:55:58 +0900

    ABC027 A問題 改良

--- ABC027/ABC027_A.cpp
+++ ABC027/ABC027_A.cpp
@@ -15,6 +15,5 @@ int main(){
     int a, b, c;
     cin >> a >> b >> c;
     if(a == b) cout << c << endl;
-    else if(a == c) cout << b << endl;
-    else cout << a << endl;
+    else if(b == c) cout << b << endl;
 }
