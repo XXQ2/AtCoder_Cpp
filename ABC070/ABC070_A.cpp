a19b5d0ae402e55d98cc471967fcf86f79cf64a2
頼本圭太 <asbo0502@gmail.com>
2020-04-15 20:37:51 +0900

    ABC070 精進 A問題(B~Dもファイルだけはアップしておく)

---
+++ ABC070/ABC070_A.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+#define forn(i, n) for (long long i = 0; i < (long long)(n); ++i)
+#define for1(i, n) for (long long i = 1; i <= (long long)(n); ++i)
+#define fore(i, l, r) for (long long i = (long long)(l); i <= (long long)(r); ++i)
+#define fordn(i, n) for (long long i = (long long)(n) - 1; i >= 0; --i)
+#define ford1(i, n) for (long long i = (long long)(n); i >= 1; --i)
+#define all(x) (x).begin(), (x).end()
+#define PI 3.141592653589793
+using namespace std;
+using ll = long long;
+using ld = long double;
+
+int main(){
+    ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+    string n;
+    cin >> n;
+    bool ans = true;
+    int s = n.size() - 1;
+    forn(i, s){
+        if(n[i] != n[s-i]) ans = false;
+    }
+    cout << (ans ? "Yes" : "No") << endl;
+}
