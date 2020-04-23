9eb6f20620a356eca98c54f2ad55a076cd011dac
頼本圭太 <asbo0502@gmail.com>
2020-04-23 09:15:45 +0900

    ABC163 精進 B問題

--- ABC163/ABC163_B.cpp
+++ ABC163/ABC163_B.cpp
@@ -12,5 +12,12 @@ using ld = long double;
 
 int main(){
     ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
-    return 0;
+    int n, m;
+    cin >> n >> m;
+    vector<int> v(m);
+    forn(i, m) cin >> v[i];
+
+    int sum = accumulate(all(v), 0);
+    if(sum > n) cout << "-1" << endl;
+    else cout << n - sum << endl;
 }
