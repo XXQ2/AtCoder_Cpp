d1d4f53451c13ab696a298ff7f761896168b3684
頼本圭太 <asbo0502@gmail.com>
2020-04-24 21:20:26 +0900

    ABC132 精進 A問題(B~Dもファイルだけはアップしておく)

---
+++ ABC132/ABC132_A.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+#define forn(i, n) for (long long i = 0; i < (long long)(n); ++i)
+#define for1(i, n) for (long long i = 1; i <= (long long)(n); ++i)
+#define fore(i, l, r) for (long long i = (long long)(l); i <= (long long)(r); ++i)
+#define fordn(i, n) for (long long i = (long long)(n) - 1; i >= 0; --i)
+#define ford1(i, n) for (long long i = (long long)(n); i >= 1; --i)
+#define all(x) (x).begin(), (x).end()
+#define PI 3.141592653589793
+using namespace std;
+using ll = long long;
+using ld = long double;
+
+int main(){
+    ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+    string s;
+    cin >> s;
+    bool ans = false;
+    if(s[0] == s[1]){
+        if(s[0] != s[2] && s[2] == s[3]) ans = true;
+    }else if(s[0] == s[2]){
+        if(s[0] != s[1] && s[1] == s[3]) ans = true;
+    }else if(s[0] == s[3]){
+        if(s[0] != s[1] && s[1] == s[2]) ans = true;
+    }
+
+    cout << (ans ? "Yes" : "No") << endl;
+}
